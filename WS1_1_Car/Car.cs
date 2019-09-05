c5e98bebf69e0dbfe746d6e9e49c84ec4a99182e
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-09-05 15:09:56 +0300

    Initial commit WS 1.1 Car

---
+++ WS1_1_Car/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WS1_1_Car
+{
+    class Car
+    {
+        // Attributes (fields)
+        // = private instance variables to store object data
+        // Visible within class
+        private string Model;
+        private string PlateNumber;
+        private int MaxSpeed;
+        private int CurrentSpeed;
+        private int FuelCapacity;
+        private int RemainingFuel;
+
+        // Constructor
+        public Car(string inputPlateNumber)
+        {
+            PlateNumber = inputPlateNumber;
+        }
+
+        // Methods
+        // Sets the current speed to this instance
+        public void SetCurrentSpeed(int targetSpeed)
+        {
+            CurrentSpeed = targetSpeed;
+        }
+
+        // Gets current speed of this instance
+        public int CheckCurrentSpeed()
+        {
+            return CurrentSpeed;
+        }
+
+        // Gets plate number of this instance
+        public string GetPlateNumber()
+        {
+            return PlateNumber;
+        }
+    }
+}
---
+++ WS1_1_Car/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WS1_1_Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Car class
+            Car firstCar = new Car("OOP-001");
+            // Set current speed
+            firstCar.SetCurrentSpeed(120);
+            // Print out info
+            Console.WriteLine($"Car { firstCar.GetPlateNumber() } is driving { firstCar.CheckCurrentSpeed() } km/h");
+            // Create another instance of the Car class
+            Car secondCar = new Car("ABC-123");
+            // Print out info
+            Console.WriteLine($"Car { secondCar.GetPlateNumber() } is driving { secondCar.CheckCurrentSpeed() } km/h");
+            // Set current speed
+            secondCar.SetCurrentSpeed(100);
+            // Print out info
+            Console.WriteLine($"Car { secondCar.GetPlateNumber() } is driving { secondCar.CheckCurrentSpeed() } km/h");
+        }
+    }
+}
