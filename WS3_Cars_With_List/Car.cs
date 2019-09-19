8a3488f15b58b4ecfb53f3d6e7d43c110b3a75bd
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-09-19 17:00:57 +0300

    Initial commit WS3_Cars_With_List

---
+++ WS3_Cars_With_List/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace WS3_Cars_With_List
+{
+
+    public class Car
+    {
+        // Attributes (fields)
+        // = private instance variables to store object data
+        // Visible within class
+        // Typically name in lowerCamelCase, starting with an underscore
+        private string _model;
+        private string plateNumber;
+        private int _maxSpeed;
+        private int _currentSpeed;
+        private int _fuelCapacity;
+        private int _remainingFuel;
+        // Let's create a constant for max speed and set it to 240.
+        // Constant names are typically spelt with uppercase letters.
+        private const int MAX_SPEED = 240;
+
+        // Constructor: no return value, not even void
+        // Same name as the class
+        // Parameters: Accepts plate number as a string as an input variable
+        public Car(string plateNumber)
+        {
+            // If the attribute and input parameter have the same name, we need to use keyword this
+            this.plateNumber = plateNumber;
+        }
+
+        // Methods
+        // Sets the current speed to this instance
+        // Returns a boolean depending on whether setting of the speed was successful or not
+        public bool SetCurrentSpeed(int targetSpeed)
+        {
+            // If the given speed is less than zero or greater than max speed let's not set it and return false
+            if (targetSpeed < 0 || targetSpeed > MAX_SPEED)
+            {
+                return false;
+            }
+            else
+            {
+                // Given speed ok -> Let's set it and return true
+                _currentSpeed = targetSpeed;
+                return true;
+            }
+        }
+
+        // Gets current speed of this instance
+        public int CheckCurrentSpeed()
+        {
+            return _currentSpeed;
+        }
+
+        // Gets plate number of this instance
+        public string GetPlateNumber()
+        {
+            return plateNumber;
+        }
+    }
+}
---
+++ WS3_Cars_With_List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic; // This is needed for lists.
+
+namespace WS3_Cars_With_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize a generic list with 3 Car objects. Use the constructor to set the plateNr. Hardcode the plate numbers to: (“HGR-987”, “EFX-395”, “EFX-395”)
+            List<Car> cars = new List<Car>(); // Here we create a new instance of List class.
+            cars.Add(new Car("HGR-987")); // To add items we use Add method.
+            cars.Add(new Car("EFX-395"));
+            cars.Add(new Car("EFX-395"));
+            //After the list is initialized, the program asks from the user via the command prompt the speed for each car, and sets the speed for the car calling the method SetCurrentSpeed
+            for (int i = 0; i < cars.Count; i++) // Instead of length, for lists we use count
+            {
+                Console.Write($"Enter the speed of car { cars[i].GetPlateNumber() }: ");
+                //If the user has provided an invalid speed (SetCurrentSpeed returns “false”), the program will notify that the speed is invalid and will ask the speed again
+                while (!cars[i].SetCurrentSpeed(int.Parse(Console.ReadLine())))
+                {
+                    Console.WriteLine("Invalid speed!");
+                    Console.Write($"Enter the speed of car { cars[i].GetPlateNumber() }: ");
+                }
+            }
+            //After all the speeds are successfully set, the program will calculate the speed average of all three cars and print to the screen:“The average speed of cars is xxx Km/h”
+            int averageSpeed = 0;
+            for (int i = 0; i < cars.Count; i++)
+            {
+                // Let's sum up all car speeds
+                averageSpeed += cars[i].CheckCurrentSpeed();
+            }
+            // Let's divide the sum by the number of cars.
+            averageSpeed = averageSpeed / cars.Count;
+            Console.WriteLine($"The average speed of cars is { averageSpeed } Km/h");
+        }
+    }
+}
